887af00b3753f798b950ec92a352dc06a85dfe72
HoangKyPham <hoangkypham1682@gmail.com>
2024-06-12 10:35:17 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 // vite.config.js
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-            },
-        },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        shop: resolve(__dirname, './shop.html'),
+        datail: resolve(__dirname, './product-detail.html'),
+        cart: resolve(__dirname, './cart.html'),
+      },
     },
-});
+  },
+})
